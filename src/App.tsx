52059cfaa2ad2e58a5a20d42dcee3ad6e7411570
ziga molicnik <zmolicnik@newstore.com>
2024-12-21 12:28:14 +0100

    refactor: Make incidents page the main route
    - Move incidents page to root route
    - Simplify navigation
    - Remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages';
-import IncidentsPage from './pages/incidents';
+import IncidentsPage from './pages';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-900">
-        <nav className="bg-gray-800 p-4">
-          <div className="max-w-3xl mx-auto flex gap-4">
-            <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-            <Link to="/incidents" className="text-white hover:text-gray-300">Incidents</Link>
-          </div>
-        </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/incidents" element={<IncidentsPage />} />
+          <Route path="/" element={<IncidentsPage />} />
         </Routes>
       </div>
     </Router>
